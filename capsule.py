c94705de2a1b1ca79071d78516cc2f96aaf6ae9c
Maximilian Samsinger <max@138.232.65.55.uibk.ac.at>
2018-01-15 12:50:37 +0100

    Copyright update.

--- capsule.py
+++ capsule.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017 Chris Wendler
+Copyright 2017 Chris Wendler, Maximilian Samsinger
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
